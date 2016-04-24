133525fd9a49959f6e4a72b32a1c086678f44f3d
Christopher McLean <havok2905@gmail.com>
2016-04-24 16:31:49 -0400

    updtaed places

--- src/components/place-component.js
+++ src/components/place-component.js
@@ -7,15 +7,19 @@ class Place extends React.Component {
     super();
   }
 
+  cover() {
+    return `http://quiet-thicket-99975.herokuapp.com/assets/${this.props.place.cover}`;
+  }
+
   render() {
     return (
       <div>
-        <img src='http://www.fillmurray.com/350/275'/>
+        <img src={ this.cover() } alt={ this.props.place.name }/>
         <div class='place-list__content'>
           <p><strong>{ this.props.place.name }</strong></p>
           <p>{ this.props.place.description }</p>
           <p><strong>{ this.props.place.street }, { this.props.place.city } { this.props.place.state }, { this.props.place.zip }</strong></p>
-          <p><a href=''>Website</a></p>
+          <p><a href={ this.props.place.website }>Website</a></p>
         </div>
       </div>
     );
